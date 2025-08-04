210905645aa1fadf21c36b3a498733a458be1eab
Ruslan Priadko <r.priadko@ukr.net>
2025-08-04 09:52:55 +0300

    car.drivers.set in form conditioned

--- taxi/forms.py
+++ taxi/forms.py
@@ -39,7 +39,7 @@ class CarCreationForm(forms.ModelForm):
         if commit:
             car.save()
             self.save_m2m()
-        car.drivers.set(self.cleaned_data["assigned_drivers"])
+            car.drivers.set(self.cleaned_data["assigned_drivers"])
 
         return car
 
